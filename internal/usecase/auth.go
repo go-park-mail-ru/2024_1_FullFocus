36ccef091e1b4835726429b2e3c533e191b27c28
Ilya <ilya.kovalev001@gmail.com>
2024-02-26 23:28:19 +0300

    [+]: business logic level

--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -1,23 +1,54 @@
 package usecase
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
+	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/repository"
+	"github.com/pkg/errors"
+	"log"
+	"strconv"
+)
+
+var ErrWrongPassword = errors.New("wrong password")
+
 type AuthUsecase struct {
-	repo AuthRepo
+	userRepo    repository.Users
+	sessionRepo repository.Sessions
 }
 
-type AuthRepo interface {
-	Login(login string, password string) (string, error)
-	Signup(login string, password string) (string, uint, error)
-	IsLoggedIn(login string) (bool, error)
+func (u *AuthUsecase) Login(login string, password string) (string, error) {
+	user, err := u.userRepo.GetUser(login)
+	if err != nil {
+		return "", err
+	}
+	if password != user.Password {
+		return "", ErrWrongPassword
+	}
+	return u.sessionRepo.CreateSession(login, user.ID), nil
 }
 
-func (uc *AuthUsecase) Login(login string, password string) (string, error) {
-	return uc.repo.Login(login, password)
+func (u *AuthUsecase) Signup(login string, password string) (string, string, error) {
+	user := models.User{
+		Username: login,
+		Password: password,
+	}
+	uID, err := u.userRepo.CreateUser(user)
+	if err != nil {
+		return "", "", err
+	}
+	sID := u.sessionRepo.CreateSession(login, uID)
+
+	uIDHash := md5.Sum([]byte(strconv.Itoa(int(uID))))
+	stringUID := hex.EncodeToString(uIDHash[:])
+	log.Printf("User ID hash: %s", stringUID)
+	return sID, stringUID, nil
 }
 
-func (uc *AuthUsecase) Signup(login string, password string) (string, uint, error) {
-	return uc.repo.Signup(login, password)
+func (u *AuthUsecase) Logout(login string) error {
+	return u.sessionRepo.DeleteSession(login)
 }
 
-func (uc *AuthUsecase) IsLoggedIn(login string) (bool, error) {
-	return uc.repo.IsLoggedIn(login)
+func (u *AuthUsecase) IsLoggedIn(login string) bool {
+	return u.sessionRepo.SessionExists(login)
 }
--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,1 +1,7 @@
 package usecase
+
+type Auth interface {
+	Login(login string, password string) (string, error)
+	Signup(login string, password string) (string, string, error)
+	Logout(login string) error
+}
