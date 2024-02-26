9116ebcc54256e58c2b5905876b1f3d5d5119368
Ilya <ilya.kovalev001@gmail.com>
2024-02-26 23:26:13 +0300

    [+]: storage level

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,14 @@
 package repository
+
+import "github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
+
+type Users interface {
+	CreateUser(user models.User) (uint, error)
+	GetUser(login string) (models.User, error)
+}
+
+type Sessions interface {
+	CreateSession(login string, userID uint) string
+	SessionExists(login string) bool
+	DeleteSession(login string) error
+}
--- internal/repository/session.go
+++ internal/repository/session.go
@@ -1,96 +1,45 @@
-package session
+package repository
 
 import (
+	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/pkg/uuidgen"
 	"github.com/pkg/errors"
-	"math/rand"
 	"sync"
-
-	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
 )
 
 var (
-	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	ErrNoSession = errors.New("no session")
 )
 
-var (
-	ErrNoUser            = errors.New("no user")
-	ErrUserAlreadyExists = errors.New("user exists")
-	ErrNoSession         = errors.New("no session")
-)
-
-func RandStringRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
-
-type Handler struct {
+type SessionRepo struct {
 	*sync.Mutex
 	sessions map[string]uint
-	users    *users
 }
 
-type users struct {
-	storage map[string]models.User
-	nextID  uint
-}
-
-func NewHandler() *Handler {
-	return &Handler{
+func NewSessionRepo() *SessionRepo {
+	return &SessionRepo{
 		sessions: make(map[string]uint, 10),
-		users:    newUsers(),
 	}
 }
 
-func newUsers() *users {
-	return &users{
-		storage: make(map[string]models.User),
-	}
-}
-
-func (h *Handler) CreateUser(user models.User) (uint, error) {
-	h.Lock()
-	defer h.Unlock()
-	if _, ok := h.users.storage[user.Username]; ok {
-		return 0, ErrUserAlreadyExists
-	}
-	h.users.nextID++
-	user.ID = h.users.nextID
-	h.users.storage[user.Username] = user
-	return user.ID, nil
-}
-
-func (h *Handler) GetUser(login string) (models.User, error) {
-	h.Lock()
-	user, ok := h.users.storage[login]
-	h.Unlock()
-	if !ok {
-		return models.User{}, ErrNoUser
-	}
-	return user, nil
-}
-
-func (h *Handler) CreateSession(login string, userID uint) string {
-	sID := RandStringRunes(32)
-	h.Lock()
-	h.sessions[sID] = userID
-	h.Unlock()
+func (r *SessionRepo) CreateSession(login string, userID uint) string {
+	sID := uuidgen.RandStringRunes(32)
+	r.Lock()
+	r.sessions[sID] = userID
+	r.Unlock()
 	return sID
 }
 
-func (h *Handler) SessionExists(login string) bool {
-	_, ok := h.sessions[login]
+func (r *SessionRepo) SessionExists(login string) bool {
+	_, ok := r.sessions[login]
 	return ok
 }
 
-func (h *Handler) DeleteSession(login string) error {
-	h.Lock()
-	defer h.Unlock()
-	if _, ok := h.sessions[login]; !ok {
+func (r *SessionRepo) DeleteSession(login string) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.sessions[login]; !ok {
 		return ErrNoSession
 	}
-	delete(h.sessions, login)
+	delete(r.sessions, login)
 	return nil
 }
