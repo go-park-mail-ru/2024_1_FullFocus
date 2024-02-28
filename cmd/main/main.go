ff9de2092e420f95c754511ba476a97c39bd4d0f
Ilya <ilya.kovalev001@gmail.com>
2024-02-28 22:01:58 +0300

    [+]: CORS

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 	"os"
@@ -13,16 +12,10 @@ import (
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/repository"
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/usecase"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func run() {
-
-	// Layers init
-
-	userRepo := repository.NewUserRepo()
-	sessionRepo := repository.NewSessionRepo()
-	authUsecase := usecase.NewAuthUsecase(userRepo, sessionRepo)
-	authHandler := delivery.NewAuthHandler(authUsecase)
 
 	// Router init
 
@@ -31,30 +24,37 @@ func run() {
 		http.Error(w, `Not found`, 404)
 	})
 
-	auth := r.PathPrefix("/auth").Subrouter()
-	{
-		auth.Handle("/login", http.HandlerFunc(authHandler.Login)).Methods("GET", "POST", "OPTIONS")
-		auth.Handle("/signup", http.HandlerFunc(authHandler.Signup)).Methods("GET", "POST", "OPTIONS")
-		auth.Handle("/logout", http.HandlerFunc(authHandler.Logout)).Methods("GET", "OPTIONS")
-	}
-	http.Handle("/", r)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(r)
 
 	// Server init
 
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:              ":8080",
-		Handler:           r,
+		Handler:           handler,
 		ReadTimeout:       1 * time.Second,
 		WriteTimeout:      1 * time.Second,
 		IdleTimeout:       30 * time.Second,
 		ReadHeaderTimeout: 2 * time.Second,
 	}
 
+	// Layers init
+
+	userRepo := repository.NewUserRepo()
+	sessionRepo := repository.NewSessionRepo()
+	authUsecase := usecase.NewAuthUsecase(userRepo, sessionRepo)
+	authHandler := delivery.NewAuthHandler(srv, authUsecase)
+	authHandler.InitRouter(r)
+
 	// Run server
 
 	go func() {
 		log.Printf("server is running...")
-		if err := srv.ListenAndServe(); err != nil {
+		if err := authHandler.Run(); err != nil {
 			log.Printf("HTTP server ListenAndServe Error: %s", err.Error())
 		}
 	}()
@@ -67,7 +67,7 @@ func run() {
 	<-exit
 
 	log.Printf("shutting down...")
-	if err := srv.Shutdown(context.Background()); err != nil {
+	if err := authHandler.Stop(); err != nil {
 		log.Printf("HTTP Server Shutdown Error: %v", err)
 	}
 }
