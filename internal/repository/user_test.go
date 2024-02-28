d80a3cb376ff503190f63166d2fe203a01bff56f
Artem-Mo <m0gilevskiy@yandex.ru>
2024-02-28 21:17:58 +0300

    [-]: ptr removed

--- internal/repository/user_test.go
+++ internal/repository/user_test.go
@@ -22,12 +22,12 @@ func TestCreateUser(t *testing.T) {
 		Password: "test",
 	}
 	t.Run("Check new user creation", func(t *testing.T) {
-		if _, err := ur.CreateUser(&testUser1); err != nil {
+		if _, err := ur.CreateUser(testUser1); err != nil {
 			t.Errorf("User not created")
 		}
 	})
 	t.Run("Check duplicate username creation", func(t *testing.T) {
-		if _, err := ur.CreateUser(&testUser1); err != ErrUserAlreadyExists {
+		if _, err := ur.CreateUser(testUser1); err != ErrUserAlreadyExists {
 			t.Errorf("Duplicate username user created")
 		}
 	})
@@ -40,7 +40,7 @@ func TestGetUser(t *testing.T) {
 		Password: "test",
 	}
 	t.Run("Check existing user error", func(t *testing.T) {
-		ur.CreateUser(&testUser1)
+		ur.CreateUser(testUser1)
 		if _, err := ur.GetUser(testUser1.Username); err != nil {
 			t.Errorf("Got incorrect result")
 		}
@@ -50,7 +50,7 @@ func TestGetUser(t *testing.T) {
 		Password: "test1",
 	}
 	t.Run("Check existing user error", func(t *testing.T) {
-		uid, _ := ur.CreateUser(&testUser2)
+		uid, _ := ur.CreateUser(testUser2)
 		if gotUser, _ := ur.GetUser(testUser2.Username); gotUser.ID != uid {
 			t.Errorf("Got incorrect result")
 		}
