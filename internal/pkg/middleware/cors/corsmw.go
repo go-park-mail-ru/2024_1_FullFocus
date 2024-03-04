858c708a26e8da880b543aa89f0f58423029aec1
Ilya <ilya.kovalev001@gmail.com>
2024-03-04 23:05:34 +0300

    [*]: all origins allowed

--- internal/pkg/middleware/cors/corsmw.go
+++ internal/pkg/middleware/cors/corsmw.go
@@ -1,21 +1,19 @@
 package middleware
 
 import (
-	"net/http"
-	"slices"
-
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func CORSMiddleware(allowedHosts []string) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
-			if slices.Contains(allowedHosts, origin) {
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-			}
+			//if slices.Contains(allowedHosts, origin) {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+			//}
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
 				return
