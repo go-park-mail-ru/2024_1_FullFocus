49f8fa8ce394a0540bb20b2aa2e9a8ea5cf0f513
Ilya <ilya.kovalev001@gmail.com>
2024-03-16 02:21:49 +0300

    [*]: logger + ctx funtions moved to helper

---
+++ internal/pkg/helper/logger.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/pkg/logger"
+)
+
+type ctxLogger struct{}
+
+// ContextWithLogger adds logger to context
+func ContextWithLogger(ctx context.Context, l *slog.Logger) context.Context {
+	return context.WithValue(ctx, ctxLogger{}, l)
+}
+
+// GetLoggerFromContext returns logger from context
+func GetLoggerFromContext(ctx context.Context) *slog.Logger {
+	if l, ok := ctx.Value(ctxLogger{}).(*slog.Logger); ok {
+		return l
+	}
+	return logger.NewLogger()
+}
