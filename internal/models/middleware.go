2dbf9b7ca59203b24e1afac106fef5466d39f581
Ilya <ilya.kovalev001@gmail.com>
2024-03-16 02:26:57 +0300

    [*]: files rename

--- internal/models/middleware.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type UserKey string
-
-const ContextUserKey UserKey = "user_id"
--- internal/pkg/helper/uidgetter.go
+++ internal/pkg/helper/uidgetter.go
@@ -6,8 +6,10 @@ import (
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
 )
 
+type userId struct{}
+
 func GetUserIDFromContext(ctx context.Context) (uint, error) {
-	userID, ok := ctx.Value(models.ContextUserKey).(uint)
+	userID, ok := ctx.Value(userId{}).(uint)
 	if !ok {
 		return 0, models.ErrNoUserID
 	}
