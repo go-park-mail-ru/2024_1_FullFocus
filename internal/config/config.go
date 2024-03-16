f3e21ff4a7d811e2ad699ac373bdada6f382b523
Ilya <ilya.kovalev001@gmail.com>
2024-03-16 16:46:56 +0300

    [+]: config parse implementation

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,47 @@
 package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/pkg/errors"
+)
+
+type Config struct {
+	Env        string        `yaml:"env" env-required:"true"`
+	SessionTTL time.Duration `yaml:"session_ttl"`
+	Server     ServerConfig  `yaml:"server"`
+}
+
+type ServerConfig struct {
+	Port        string        `yaml:"port"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+func MustLoad() *Config {
+	path := parseConfigPath()
+	if path == "" {
+		panic("config path not specified")
+	}
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		panic("config file does not exist")
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("error while reading config: " + err.Error())
+	}
+	return &cfg
+}
+
+func parseConfigPath() string {
+	var path string
+	flag.StringVar(&path, "config_path", "", "path to config file")
+	flag.Parse()
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+	return path
+}
