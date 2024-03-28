29e3c0abf8e6dd34b945e0fb6dd237e58d5c7dd6
Ilya <ilya.kovalev001@gmail.com>
2024-03-28 21:51:09 +0300

    [+]: minio client

--- pkg/minio/minio.go
+++ pkg/minio/minio.go
@@ -1,1 +1,15 @@
 package minio
+
+import (
+	"fmt"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+
+	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/config"
+)
+
+func NewClient(cfg config.MinioConfig) (*minio.Client, error) {
+	return minio.New(fmt.Sprintf("127.0.0.1%s", cfg.Port), &minio.Options{
+		Creds: credentials.NewStaticV4(cfg.MinioUser, cfg.MinioPassword, ""),
+	})
+}
