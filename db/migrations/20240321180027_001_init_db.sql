e0392d1e45967b7755023ac69cc4ee2deccf77af
Ilya <ilya.kovalev001@gmail.com>
2024-04-08 18:48:10 +0300

    ...

--- db/migrations/20240321180027_001_init_db.sql
+++ db/migrations/20240321180027_001_init_db.sql
@@ -9,9 +9,9 @@ SET search_path TO ozon, public;
 -- ozon.user definition
 
 CREATE TABLE default_user (
-	id serial PRIMARY KEY,
-	user_login text NOT NULL UNIQUE CHECK (char_length(user_login) BETWEEN 4 AND 32),
-	password_hash text NOT NULL,
+	id SERIAL PRIMARY KEY,
+	user_login TEXT NOT NULL UNIQUE CHECK (char_length(user_login) BETWEEN 4 AND 32),
+	password_hash TEXT NOT NULL CHECK (char_length(user_login) BETWEEN 8 AND 255),
 	created_at TIMESTAMP DEFAULT now() NOT NULL,
 	updated_at TIMESTAMP DEFAULT now() NOT NULL
 );
@@ -20,10 +20,10 @@ CREATE TABLE default_user (
 
 CREATE TABLE user_profile (
 	id int4 PRIMARY KEY REFERENCES default_user(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	full_name text NOT NULL CHECK (char_length(full_name) BETWEEN 1 AND 255),
-	email text NOT NULL CHECK ((char_length(email) BETWEEN 4 AND 255) AND (email ~* '^[a-z0-9\.\-]+@[a-z0-9\.\-]+\.[a-z]+$')),
-	phone_number text NOT NULL CHECK ((char_length(phone_number) BETWEEN 5 AND 15) AND (phone_number ~ '\+?[0-9]+')),
-	imgsrc text,
+	full_name TEXT NOT NULL CHECK (char_length(full_name) BETWEEN 1 AND 255),
+	email TEXT NOT NULL CHECK ((char_length(email) BETWEEN 4 AND 255) AND (email ~* '^[a-z0-9\.\-]+@[a-z0-9\.\-]+\.[a-z]+$')),
+	phone_number TEXT NOT NULL CHECK ((char_length(phone_number) BETWEEN 5 AND 15) AND (phone_number ~ '\+?[0-9]+')),
+	imgsrc TEXT DEFAULT '',
 	created_at TIMESTAMP DEFAULT now() NOT NULL,
 	updated_at TIMESTAMP DEFAULT now() NOT NULL
 );
@@ -31,13 +31,13 @@ CREATE TABLE user_profile (
 -- ozon.product definition
 
 CREATE TABLE product (
-	id serial PRIMARY KEY,
-	product_name text NOT NULL CHECK (char_length(product_name) BETWEEN 1 AND 50),
-	product_description text NULL CHECK (char_length(product_description) BETWEEN 1 AND 255),
+	id SERIAL PRIMARY KEY,
+	product_name TEXT NOT NULL CHECK (char_length(product_name) BETWEEN 1 AND 50),
+	product_description TEXT DEFAULT NULL CHECK (char_length(product_description) BETWEEN 1 AND 255),
 	price int4 NOT NULL CHECK (price > 0),
-	imgsrc text,
-	seller text NOT NULL,
-	rating int2 DEFAULT 0 NOT NULL,
+	imgsrc TEXT DEFAULT '',
+	seller TEXT NOT NULL CHECK (char_length(seller) > 0),
+	rating int2 DEFAULT 0 NOT NULL CHECK (rating >= 0),
 	created_at TIMESTAMP DEFAULT now() NOT NULL,
 	updated_at TIMESTAMP DEFAULT now() NOT NULL
 );
@@ -45,8 +45,8 @@ CREATE TABLE product (
 -- ozon.category definition
 
 CREATE TABLE category (
-	id smallserial PRIMARY KEY,
-	category_name text NOT NULL UNIQUE CHECK (char_length(category_name) BETWEEN 1 AND 50),
+	id SMALLSERIAL PRIMARY KEY,
+	category_name TEXT NOT NULL UNIQUE CHECK (char_length(category_name) BETWEEN 1 AND 50),
 	parent_id int2 NOT NULL REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -72,7 +72,7 @@ CREATE TYPE ordering_status AS ENUM (
 CREATE TABLE ordering (
 	id bigserial PRIMARY KEY,
 	sum int4 DEFAULT 0 NOT NULL CHECK (sum >= 0),
-	profile_id int4 NOT NULL REFERENCES user_profile(id),
+	profile_id int4 NOT NULL REFERENCES user_profile(id) ON DELETE CASCADE ON UPDATE CASCADE ,
 	order_status ordering_status NOT NULL,
 	created_at TIMESTAMP DEFAULT now() NOT NULL,
 	updated_at TIMESTAMP DEFAULT now() NOT NULL
@@ -82,7 +82,7 @@ CREATE TABLE ordering (
 
 CREATE TABLE order_item (
 	ordering_id int8 NOT NULL REFERENCES ordering(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	product_id int4 NOT NULL REFERENCES product(id),
+	product_id int4 NOT NULL REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	count int2 DEFAULT 1 NOT NULL CHECK (count > 0),
 	created_at TIMESTAMP DEFAULT now() NOT NULL,
 	updated_at TIMESTAMP DEFAULT now() NOT NULL,
