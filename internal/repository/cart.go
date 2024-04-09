3bee6622f54afe025de5b4119acd8b3a6c6d4098
Artem-Mo <m0gilevskiy@yandex.ru>
2024-04-09 15:02:56 +0300

    [+]: query logs on db level

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
-	"log/slog"
-	"time"
 
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
 	db "github.com/go-park-mail-ru/2024_1_FullFocus/internal/pkg/database"
@@ -29,12 +26,6 @@ func (r *CartRepo) GetAllCartItems(ctx context.Context, uID uint) ([]models.Cart
 	FROM product AS p JOIN cart_item AS c ON p.id = c.product_id
 	WHERE profile_id = $1;`
 
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d", uID)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("queried in %s", time.Since(start)))
-	}()
-
 	cartProductRows := []dao.CartProductTable{}
 	err := r.storage.Select(ctx, &cartProductRows, q, uID)
 	if err != nil {
@@ -50,12 +41,6 @@ func (r *CartRepo) GetAllCartItems(ctx context.Context, uID uint) ([]models.Cart
 
 func (r *CartRepo) GetAllCartItemsID(ctx context.Context, uID uint) ([]models.CartItem, error) {
 	q := `SELECT product_id, count FROM cart_item WHERE profile_id = $1;`
-
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d", uID)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("queried in %s", time.Since(start)))
-	}()
 
 	cartItemRows := []dao.CartItemTable{}
 	err := r.storage.Select(ctx, &cartItemRows, q, uID)
@@ -76,12 +61,6 @@ func (r *CartRepo) UpdateCartItem(ctx context.Context, uID, prID uint) (uint, er
 	DO UPDATE set count = cart_item.count + 1
 	returning cart_item.count AS count;`
 
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d $2 = %d", uID, prID)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("updated in %s", time.Since(start)))
-	}()
-
 	resRow := dao.CartItemTable{}
 	err := r.storage.Get(ctx, &resRow, q, uID, prID)
 	if err != nil {
@@ -100,12 +79,6 @@ func (r *CartRepo) DeleteCartItem(ctx context.Context, uID, prID uint) (uint, er
 	WHERE profile_id = $1 AND product_id = $2
 	returning cart_item.count AS count;`
 
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d $2 = %d", uID, prID)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("updated in %s", time.Since(start)))
-	}()
-
 	resRow := dao.CartItemTable{}
 	err := r.storage.Get(ctx, &resRow, q, uID, prID)
 	if err != nil {
@@ -120,12 +93,6 @@ func (r *CartRepo) DeleteCartItem(ctx context.Context, uID, prID uint) (uint, er
 	if resRow.Count == 0 {
 		q = `DELETE FROM cart_item WHERE profile_id = $1 AND product_id = $2;`
 
-		logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d $2 = %d", uID, prID)))
-		start = time.Now()
-		defer func() {
-			logger.Info(ctx, fmt.Sprintf("deleted in %s", time.Since(start)))
-		}()
-
 		if _, err = r.storage.Exec(ctx, q, uID, prID); err != nil {
 			return 0, models.ErrNoProduct
 		}
@@ -136,12 +103,6 @@ func (r *CartRepo) DeleteCartItem(ctx context.Context, uID, prID uint) (uint, er
 
 func (r *CartRepo) DeleteAllCartItems(ctx context.Context, uID uint) error {
 	q := `DELETE FROM cart_item WHERE profile_id = $1;`
-
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %d", uID)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("deleted in %s", time.Since(start)))
-	}()
 
 	res, err := r.storage.Exec(ctx, q, uID)
 	if err != nil {
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,9 +2,6 @@ package repository
 
 import (
 	"context"
-	"fmt"
-	"log/slog"
-	"time"
 
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/models"
 	db "github.com/go-park-mail-ru/2024_1_FullFocus/internal/pkg/database"
@@ -26,12 +23,6 @@ func (r *UserRepo) CreateUser(ctx context.Context, user models.User) (uint, erro
 	userRow := dao.ConvertUserToTable(user)
 	q := `INSERT INTO default_user (user_login, password_hash) VALUES ($1, $2) returning id;`
 
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %s", userRow.Login)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("created in %s", time.Since(start)))
-	}()
-
 	resRow := dao.UserTable{}
 	err := r.storage.Get(ctx, &resRow, q, userRow.Login, userRow.PasswordHash)
 	if err != nil {
@@ -43,11 +34,7 @@ func (r *UserRepo) CreateUser(ctx context.Context, user models.User) (uint, erro
 
 func (r *UserRepo) GetUser(ctx context.Context, username string) (models.User, error) {
 	q := `SELECT id, password_hash FROM default_user WHERE user_login = $1;`
-	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1 = %s", username)))
-	start := time.Now()
-	defer func() {
-		logger.Info(ctx, fmt.Sprintf("queried in %s", time.Since(start)))
-	}()
+
 	userRow := &dao.UserTable{}
 	if err := r.storage.Get(ctx, userRow, q, username); err != nil {
 		logger.Error(ctx, "user not found")
