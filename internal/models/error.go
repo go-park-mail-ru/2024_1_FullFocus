b631578eb2525f1067cdcdbd8bb7ed63b084b719
Artem-Mo <m0gilevskiy@yandex.ru>
2024-04-10 00:54:02 +0300

    [*]: linter fixes

--- internal/models/error.go
+++ internal/models/error.go
@@ -16,5 +16,5 @@ var (
 	ErrNoProfile            = errors.New("no profile")
 	ErrProfileAlreadyExists = errors.New("profile exists")
 	ErrEmptyCart            = errors.New("no cart items found")
-  ErrInternal             = errors.New("internal server error")
+	ErrInternal             = errors.New("internal server error")
 )
