00d9691fe264dd0d005058ec0931ce40e4df0a5b
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-04-09 17:46:22 +0300

    Функции для загрузки аватарок

--- internal/repository/profile.go
+++ internal/repository/profile.go
@@ -65,7 +65,7 @@ func (r *ProfileRepo) GetProfile(ctx context.Context, uID uint) (models.Profile,
 
 func (r *ProfileRepo) UpdateProfile(ctx context.Context, uID uint, profileNew models.Profile) error {
 	profileRow := db.ConvertProfileToTable(profileNew)
-	q := `UPDATE ozon.user_profile SET full_name=$1, email=$2, phone_number=$3, imgsrc=$4 WHERE id = $5 RETURNING id`
+	q := `UPDATE ozon.user_profile SET full_name=$1, email=$2, phone_number=$3, imgsrc=$4 WHERE id = $5 RETURNING id;`
 	start := time.Now()
 	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1=%s, $2=%s, $3=%s, $4=%s, $5=%d", profileRow.FullName, profileRow.Email, profileRow.PhoneNumber, profileRow.ImgSrc, uID)))
 	reqProfileRow := &db.ProfileTable{}
@@ -89,4 +89,69 @@ func (r *ProfileRepo) UpdateProfile(ctx context.Context, uID uint, profileNew mo
 	logger.Info(ctx, fmt.Sprintf("updated in %s", time.Since(start)))
 
 	return nil
+}
+
+func (r *ProfileRepo) UpdateAvatarByProfileID(ctx context.Context, uID uint, imgSrc string) error {
+	q := `UPDATE ozon.user_profile SET imgsrc=$1 WHERE id = $2 RETURNING id;` // Чтобы get ловил отсутствие пользователя
+	start := time.Now()
+	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1=%s, $2=%d", imgSrc, uID)))
+	reqProfileRow := &db.ProfileTable{}
+	err := r.storage.Get(ctx, reqProfileRow, q,
+		imgSrc,
+		uID)
+	if err != nil {
+		if pgErr := new(pgconn.PgError); errors.As(err, &pgErr) {
+			logger.Error(ctx, fmt.Sprintf("HERE pg err: %v", err))
+		} else if reqProfileRow.ID == 0 {
+			logger.Error(ctx, fmt.Sprintf("HERE profile not found: %v", err))
+		}
+		return models.ErrNoProfile
+	}
+	logger.Info(ctx, fmt.Sprintf("updated in %s", time.Since(start)))
+	return nil
+}
+
+func (r *ProfileRepo) GetAvatarByProfileID(ctx context.Context, uID uint) (string, error) {
+	q := `SELECT imgsrc, id FROM ozon.user_profile WHERE id = $1;`
+	start := time.Now()
+	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1=%d", uID)))
+	reqProfileRow := &db.ProfileTable{}
+	err := r.storage.Get(ctx, reqProfileRow, q, uID)
+	if err != nil {
+		if pgErr := new(pgconn.PgError); errors.As(err, &pgErr) {
+			logger.Error(ctx, fmt.Sprintf("HERE pg err: %v", err))
+		}
+		return "", models.ErrNoProfile
+	}
+
+	if reqProfileRow.ID != uID { // Запрос исполнился, но по факту пользователя нет
+		logger.Error(ctx, "profile not found")
+		return "", models.ErrNoProfile
+	}
+
+	logger.Info(ctx, fmt.Sprintf("geted in %s", time.Since(start)))
+	return reqProfileRow.ImgSrc, nil
+}
+
+func (r *ProfileRepo) DeleteAvatarByProfileID(ctx context.Context, uID uint) (string, error) {
+	imgSrc, err := r.GetAvatarByProfileID(ctx, uID)
+	if err != nil {
+		return "", err
+	}
+	if imgSrc == "" {
+		return "", models.ErrNoAvatar
+	}
+
+	q := `UPDATE user_profile SET imgsrc = '' WHERE id = $1;`
+	start := time.Now()
+	logger.Info(ctx, q, slog.String("args", fmt.Sprintf("$1=%d", uID)))
+	_, err = r.storage.Exec(ctx, q, uID)
+	if err != nil {
+		if pgErr := new(pgconn.PgError); errors.As(err, &pgErr) {
+			logger.Error(ctx, fmt.Sprintf("HERE pg err: %v", err))
+		}
+		return "", models.ErrNoProfile
+	}
+	logger.Info(ctx, fmt.Sprintf("updated in %s", time.Since(start)))
+	return imgSrc, nil
 }
