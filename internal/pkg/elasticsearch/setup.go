c7895756f5d1e15962c6f7b86f08375783d6eb5c
Ilya <ilya.kovalev001@gmail.com>
2024-04-22 04:39:17 +0300

    [*]: setup cleaned up

--- internal/pkg/elasticsearch/setup.go
+++ internal/pkg/elasticsearch/setup.go
@@ -5,30 +5,70 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"strings"
-	"sync/atomic"
-	"time"
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/elastic/go-elasticsearch/v8/esutil"
+
 	"github.com/go-park-mail-ru/2024_1_FullFocus/internal/pkg/database"
 )
 
 const (
-	_categoryIndex        = "category"
-	_productIndex         = "product"
+	CategoryIndex = "category"
+	ProductIndex  = "product"
+)
+
+const (
+	_categoryIndexSettings = `
+	{
+		"settings": {
+			"analysis": {
+				"filter": {
+					"russian_morphology": {
+						"type": "snowball",
+						"language": "russian"
+					},
+					"edge_ngram_filter": {
+						"type": "edge_ngram",
+						"min_gram": 1,
+						"max_gram": 15,
+						"preserve_original": true
+					}
+				},
+				"analyzer": {
+					"category_name_analyzer": {
+						"type": "custom",
+						"tokenizer": "standard",
+						"filter": ["lowercase", "russian_morphology", "edge_ngram_filter"]
+					}
+				}
+			}
+		},
+		"mappings": {
+			"properties": {
+				"category_id": {
+					"type": "long"
+				},
+				"category_name": {
+					"type": "text",
+					"analyzer": "category_name_analyzer",
+					"fields": {
+						"keyword": {
+							"type": "keyword",
+							"ignore_above": 256
+						}
+					}
+				}
+			}
+		}
+	}
+	`
 	_productIndexSettings = `
     {
         "settings": {
             "analysis": {
                 "filter": {
-					"shingle_filter": {
-						"type": "shingle",
-						"max_shingle_size": 4,
-						"output_unigrams": true
-					},
 					"russian_morphology": {
 						"type": "snowball",
 						"language": "russian"
@@ -38,100 +78,57 @@ const (
 					"product_name_analyzer": {
 						"type": "custom",
 						"tokenizer": "whitespace",
-						"filter": ["shingle_filter", "lowercase", "russian_morphology"]
+						"filter": ["lowercase", "russian_morphology"]
 					}
 				}
             }
         },
         "mappings": {
-            "properties": {
-                "product_name": {
-                    "type": "text",
-                    "analyzer": "product_name_analyzer"
-                }
-            }
-        }
+			"properties": {
+				"product_name": {
+					"type": "completion",
+					"analyzer": "product_name_analyzer"
+				}
+			}
+		}
     }
     `
 )
+
+const _ngramWordsAmount = 4
 
 type product struct {
 	Name string `json:"product_name"`
 }
 
-type categoryTuple struct {
-	id   uint   `db:"id"`
-	name string `db:"category_name"`
+type category struct {
+	ID   uint   `json:"category_id" db:"id"`
+	Name string `json:"category_name" db:"category_name"`
 }
 
-func CreateElasticsearchIndices(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
-	//if err := CreateCategoryIndex(ctx, db, es); err != nil {
-	//	return err
-	//}
-	return CreateProductIndex(ctx, db, es)
+func InitElasticData(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
+	if err := initCategoryIndex(ctx, db, es); err != nil {
+		return err
+	}
+	return initProductIndex(ctx, db, es)
 }
 
-func CreateCategoryIndex(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
-	if err := DropIndex(es, _categoryIndex); err != nil {
-		return err
-	}
-	var categories []categoryTuple
-	q := `SELECT id, category_name
-		  FROM category;`
-	if err := db.Select(ctx, &categories, q); err != nil {
-		return err
-	}
-	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
-		Index:  _categoryIndex,
-		Client: es,
-	})
-	if err != nil {
-		return err
-	}
-	var countSuccessful uint64
-	start := time.Now().UTC()
+// Category
 
-	for _, c := range categories {
-		data, err := json.Marshal(c)
-		if err != nil {
-			log.Fatalf("Cannot encode article %s", err)
-		}
-		err = bi.Add(
-			ctx,
-			esutil.BulkIndexerItem{
-				Action: "index",
-				Body:   bytes.NewReader(data),
-				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
-					atomic.AddUint64(&countSuccessful, 1)
-				},
-				OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
-					if err != nil {
-						log.Printf("ERROR: %s", err)
-					} else {
-						log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
-					}
-				},
-			},
-		)
-		if err != nil {
-			log.Fatalf("Unexpected error: %s", err)
-		}
+func initCategoryIndex(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
+	if err := createCategoryIndex(ctx, es); err != nil {
+		return err
 	}
-	fmt.Printf("index created (%d items) in %s\n", countSuccessful, time.Since(start))
-
-	if err = bi.Close(ctx); err != nil {
-		panic("bulk index close error: " + err.Error())
-	}
-	return nil
+	return insertCategories(ctx, db, es)
 }
 
-func CreateProductIndex(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
-	if err := DropIndex(es, _productIndex); err != nil {
+func createCategoryIndex(ctx context.Context, es *elasticsearch.Client) error {
+	if err := dropIndex(es, CategoryIndex); err != nil {
 		return err
 	}
 	req := esapi.IndicesCreateRequest{
-		Index: _productIndex,
-		Body:  strings.NewReader(_productIndexSettings),
+		Index: CategoryIndex,
+		Body:  strings.NewReader(_categoryIndexSettings),
 	}
 	res, err := req.Do(ctx, es)
 	if err != nil {
@@ -141,21 +138,28 @@ func CreateProductIndex(ctx context.Context, db database.Database, es *elasticse
 	if res.IsError() {
 		return fmt.Errorf("creating index error: %s", res.String())
 	}
-	var products []string
-	q := `SELECT product_name
-		  FROM product;`
-	if err = db.Select(ctx, &products, q); err != nil {
+	return nil
+}
+
+func insertCategories(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
+	if err := dropIndex(es, CategoryIndex); err != nil {
+		return err
+	}
+	var categories []category
+	q := `SELECT id, category_name
+		  FROM category;`
+	if err := db.Select(ctx, &categories, q); err != nil {
 		return err
 	}
 	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
-		Index:  _productIndex,
+		Index:  CategoryIndex,
 		Client: es,
 	})
 	if err != nil {
 		return err
 	}
-	for _, p := range products {
-		data, _ := json.Marshal(product{Name: p})
+	for _, c := range categories {
+		data, _ := json.Marshal(c)
 		if err = bi.Add(
 			ctx,
 			esutil.BulkIndexerItem{
@@ -169,7 +173,66 @@ func CreateProductIndex(ctx context.Context, db database.Database, es *elasticse
 	return bi.Close(ctx)
 }
 
-func DropIndex(es *elasticsearch.Client, indexName string) error {
+// Product
+
+func initProductIndex(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
+	if err := createProductIndex(ctx, es); err != nil {
+		return err
+	}
+	return insertProducts(ctx, db, es)
+}
+
+func createProductIndex(ctx context.Context, es *elasticsearch.Client) error {
+	if err := dropIndex(es, ProductIndex); err != nil {
+		return err
+	}
+	req := esapi.IndicesCreateRequest{
+		Index: ProductIndex,
+		Body:  strings.NewReader(_productIndexSettings),
+	}
+	res, err := req.Do(ctx, es)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.IsError() {
+		return fmt.Errorf("creating index error: %s", res.String())
+	}
+	return nil
+}
+
+func insertProducts(ctx context.Context, db database.Database, es *elasticsearch.Client) error {
+	var products []string
+	q := `SELECT product_name
+		  FROM product;`
+	if err := db.Select(ctx, &products, q); err != nil {
+		return err
+	}
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:  ProductIndex,
+		Client: es,
+	})
+	if err != nil {
+		return err
+	}
+	for _, p := range products {
+		for _, productNameSubstring := range ProductNameNgrams(p, _ngramWordsAmount) {
+			data, _ := json.Marshal(product{Name: productNameSubstring})
+			if err = bi.Add(
+				ctx,
+				esutil.BulkIndexerItem{
+					Action: "index",
+					Body:   bytes.NewReader(data),
+				},
+			); err != nil {
+				return err
+			}
+		}
+	}
+	return bi.Close(ctx)
+}
+
+func dropIndex(es *elasticsearch.Client, indexName string) error {
 	indexExists, err := es.Indices.Exists([]string{indexName})
 	if err != nil {
 		return err
