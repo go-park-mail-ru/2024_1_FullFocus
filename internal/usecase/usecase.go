9b4ee8380544ef07a7a219bda142cf046b174c34
Ilya <ilya.kovalev001@gmail.com>
2024-05-18 19:14:05 +0300

    [+]: interfaces extend

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -8,6 +8,12 @@ import (
 )
 
 type (
+	Avatars interface {
+		GetAvatar(ctx context.Context, fileName string) (models.Avatar, error)
+		UploadAvatar(ctx context.Context, profileID uint, img models.Avatar) error
+		DeleteAvatar(ctx context.Context, uID uint) error
+	}
+
 	Auth interface {
 		Login(ctx context.Context, login string, password string) (string, error)
 		Signup(ctx context.Context, login string, password string) (string, error)
@@ -15,33 +21,6 @@ type (
 		Logout(ctx context.Context, sID string) error
 		IsLoggedIn(ctx context.Context, isID string) bool
 		UpdatePassword(ctx context.Context, userID uint, password string, newPassword string) error
-	}
-
-	Products interface {
-		GetAllProductCards(ctx context.Context, input models.GetAllProductsInput) ([]models.ProductCard, error)
-		GetProductByID(ctx context.Context, profileID uint, productID uint) (models.Product, error)
-		GetProductsByCategoryID(ctx context.Context, input models.GetProductsByCategoryIDInput) (models.GetProductsByCategoryIDPayload, error)
-		GetProductsByQuery(ctx context.Context, input models.GetProductsByQueryInput) ([]models.ProductCard, error)
-	}
-
-	Avatars interface {
-		GetAvatar(ctx context.Context, fileName string) (models.Avatar, error)
-		UploadAvatar(ctx context.Context, profileID uint, img models.Avatar) error
-		DeleteAvatar(ctx context.Context, uID uint) error
-	}
-
-	Orders interface {
-		Create(ctx context.Context, input models.CreateOrderInput) (uint, error)
-		GetOrderByID(ctx context.Context, profileID uint, orderingID uint) (models.GetOrderPayload, error)
-		GetAllOrders(ctx context.Context, profileID uint) ([]models.Order, error)
-		Delete(ctx context.Context, profileID uint, orderingID uint) error
-	}
-
-	Profiles interface {
-		UpdateProfile(ctx context.Context, uID uint, newProfile models.ProfileUpdateInput) error
-		GetProfile(ctx context.Context, uID uint) (models.Profile, error)
-		GetProfileMetaInfo(ctx context.Context, uID uint) (models.ProfileMetaInfo, error)
-		CreateProfile(ctx context.Context, profile models.Profile) error
 	}
 
 	Carts interface {
@@ -55,9 +34,35 @@ type (
 		GetAllCategories(ctx context.Context) ([]models.Category, error)
 	}
 
+	Orders interface {
+		Create(ctx context.Context, input models.CreateOrderInput) (models.CreateOrderPayload, error)
+		GetOrderByID(ctx context.Context, profileID uint, orderingID uint) (models.GetOrderPayload, error)
+		GetAllOrders(ctx context.Context, profileID uint) ([]models.Order, error)
+		Delete(ctx context.Context, profileID uint, orderingID uint) error
+	}
+
+	Products interface {
+		GetAllProductCards(ctx context.Context, input models.GetAllProductsInput) ([]models.ProductCard, error)
+		GetProductByID(ctx context.Context, profileID uint, productID uint) (models.Product, error)
+		GetProductsByCategoryID(ctx context.Context, input models.GetProductsByCategoryIDInput) (models.GetProductsByCategoryIDPayload, error)
+		GetProductsByQuery(ctx context.Context, input models.GetProductsByQueryInput) ([]models.ProductCard, error)
+	}
+
+	Profiles interface {
+		UpdateProfile(ctx context.Context, uID uint, newProfile models.ProfileUpdateInput) error
+		GetProfile(ctx context.Context, uID uint) (models.Profile, error)
+		GetProfileMetaInfo(ctx context.Context, uID uint) (models.ProfileMetaInfo, error)
+		CreateProfile(ctx context.Context, profile models.Profile) error
+	}
+
 	Reviews interface {
 		GetProductReviews(ctx context.Context, input models.GetProductReviewsInput) ([]models.ProductReview, error)
 		CreateProductReview(ctx context.Context, uID uint, input models.CreateProductReviewInput) error
+	}
+
+	Promocodes interface {
+		GetPromocodeItemByActivationCode(ctx context.Context, pID uint, code string) (models.PromocodeActivationTerms, error)
+		GetAvailablePromocodes(ctx context.Context, profileID uint) ([]models.PromocodeItem, error)
 	}
 
 	Suggests interface {
