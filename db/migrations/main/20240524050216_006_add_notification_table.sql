55cc8263c97803ef19f1f1b4b76e5534f740cfc3
Ilya <ilya.kovalev001@gmail.com>
2024-05-24 14:29:27 +0300

    [+]: notifications table

---
+++ db/migrations/main/20240524050216_006_add_notification_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+
+CREATE TYPE notification_type AS ENUM (
+    'order_status_change'
+);
+
+CREATE TABLE IF NOT EXISTS notification (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    type notification_type NOT NULL,
+    read_status BOOLEAN DEFAULT FALSE,
+    payload JSONB NOT NULL CHECK (length(payload::TEXT) > 0)
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+DROP TYPE IF EXISTS notification_type;
+DROP TABLE IF EXISTS notification;
+
+-- +goose StatementEnd
